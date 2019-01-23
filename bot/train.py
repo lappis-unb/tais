2535699715d036db9400b1ebeaddb8d8f85ae996
Bruna Pinos <bruna.pinos@gmail.com>
2019-01-23 16:43:34 -0200

    Removes environments variables in train.py
    Signed-off-by: ArthurTemporim <arthurrtl@gmail.com>
    Signed-off-by: Bruna Pinos <bruna.pinos@gmail.com>

--- bot/train.py
+++ bot/train.py
@@ -12,16 +12,6 @@ from fallback import CustomFallbackPolicy
 from validator import Validator
 
 logger = logging.getLogger(__name__)
-NLU_THRESHOLD = float(os.getenv('NLU_THRESHOLD', 0.6))
-
-TRAINING_EPOCHS = int(os.getenv('TRAINING_EPOCHS', 20))
-BATCH_SIZE = int(os.getenv('BATCH_SIZE', 10))
-VALIDATION_SPLIT = float(os.getenv('VALIDATION_SPLIT', 0.2))
-
-
-CORE_THRESHOLD = float(os.getenv('CORE_THRESHOLD', 0.6))
-MAX_HISTORY = int(os.getenv('MAX_HISTORY', 2))
-FALLBACK_ACTION_NAME = str(os.getenv('FALLBACK_ACTION_NAME', 'utter_default'))
 
 utils.configure_colored_logging(loglevel='DEBUG')
 
