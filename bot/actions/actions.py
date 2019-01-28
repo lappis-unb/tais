2d2ad26c596d2e0484cec10f7503957bebbcd436
Bruna Moreira <brunanayaramlima@gmail.com>
2019-01-28 16:51:40 -0200

    Add custom message

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -1,9 +1,10 @@
 from rasa_core_sdk import Action
 from rasa_core_sdk.events import SlotSet
 
-class ActionGetChatbots(Action):
+class ActionTest(Action):
    def name(self):
       return "action_test"
 
    def run(self, dispatcher, tracker, domain):
       dispatcher.utter_message("Essa é uma custom action de test!")
+      dispatcher.utter_message("Nova mensagem de teste")
