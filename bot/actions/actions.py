b4bc9307c09bc3fc0252b1912101ce198fd97723
Bruna Moreira <brunanayaramlima@gmail.com>
2019-01-29 14:03:13 -0200

    Get response from SALIC API

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -1,10 +1,18 @@
 from rasa_core_sdk import Action
 from rasa_core_sdk.events import SlotSet
+import requests
 
 class ActionTest(Action):
    def name(self):
       return "action_test"
 
    def run(self, dispatcher, tracker, domain):
-      dispatcher.utter_message("Essa é uma custom action de test!")
-      dispatcher.utter_message("Nova mensagem de teste")
+        try:
+          dispatcher.utter_message("Essa é uma custom action de test!")
+          req = requests.request('GET', "http://api.salic.cultura.gov.br/v1/projetos/")
+          dispatcher.utter_message(str(req.status_code))
+        except ValueError:
+          dispatcher.utter_message(ValueError)
+          dispatcher.utter_message("----------")
+          dispatcher.utter_message("svkjdkjvdhkjfdh")
+
