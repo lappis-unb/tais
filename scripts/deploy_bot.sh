33bde0f26d469cdbd5cf176622f0c55b86ace51e
Matheus Miranda <matheusmirandalacerda@gmail.com>
2019-04-20 22:04:56 -0300

    Simplify build and deploy stages
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- scripts/deploy_bot.sh
+++ scripts/deploy_bot.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-merged_branch=$3
-
-if [[ ("$(git diff "remotes/origin/$merged_branch" remotes/origin/master -- . ':!web' | wc -l)" -ge 1) ]]; then
-	sshpass  -p $1 ssh -o StrictHostKeyChecking=no root@$2 <<-'ENDSSH'
-			cd rouana/
-	    docker-compose -f ./beta.docker-compose.yml stop bot
-			docker-compose -f ./beta.docker-compose.yml rm -f bot
-	    docker rmi -f registry.gitlab.com/lappis-unb/services/tais/bot
-	    docker-compose -f ./beta.docker-compose.yml up -d bot
-	ENDSSH
-fi
+sshpass  -p $1 ssh -o StrictHostKeyChecking=no root@$2 <<-'ENDSSH'
+		cd rouana/
+    docker-compose -f ./beta.docker-compose.yml stop bot
+		docker-compose -f ./beta.docker-compose.yml rm -f bot
+    docker rmi -f registry.gitlab.com/lappis-unb/services/tais/bot
+    docker-compose -f ./beta.docker-compose.yml up -d bot
+ENDSSH
--- scripts/deploy_web.sh
+++ scripts/deploy_web.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-merged_branch=$3
-
-if [[ ("$(git diff "remotes/origin/$merged_branch" remotes/origin/master -- web | wc -l)" -ge 1) ]]; then
-	sshpass  -p $1 ssh -o StrictHostKeyChecking=no root@$2 <<-'ENDSSH'
-			cd rouana/
-	    docker-compose -f ./beta.docker-compose.yml stop web
-			docker-compose -f ./beta.docker-compose.yml rm -f web
-	    docker rmi -f registry.gitlab.com/lappis-unb/services/tais/web
-	    docker-compose -f ./beta.docker-compose.yml up -d web
-	ENDSSH
-fi
+sshpass  -p $1 ssh -o StrictHostKeyChecking=no root@$2 <<-'ENDSSH'
+		cd rouana/
+    docker-compose -f ./beta.docker-compose.yml stop web
+		docker-compose -f ./beta.docker-compose.yml rm -f web
+    docker rmi -f registry.gitlab.com/lappis-unb/services/tais/web
+    docker-compose -f ./beta.docker-compose.yml up -d web
+ENDSSH
