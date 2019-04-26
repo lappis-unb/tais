7c19627f4db18f721191568f2dd866760355c9d5
guilacerda <guiglacerda@gmail.com>
2019-04-26 18:48:25 -0300

    Removes maintenance page
    Was removed because it referred to Thais's chat

--- kibana-web/tais/urls.py
+++ kibana-web/tais/urls.py
@@ -11,7 +11,7 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls')).
 """
 from django.contrib import admin
 from django.urls import path, include
--- kibana-web/tais/views.py
+++ kibana-web/tais/views.py
@@ -1,17 +1,8 @@
 from django.shortcuts import render
-from information.models import Maintainance
 
 
 def index(request):
-    maintainance = Maintainance.objects.all().first()
-    maintainance_is_set = False
-
-    if maintainance is not None:
-        maintainance_is_set = maintainance.set_maintainance_page
-
-    return render(request, 'home.html', {
-        'maintainance_is_set': maintainance_is_set,
-    })
+    return render(request, 'home.html')
 
 
 def trending(request):
