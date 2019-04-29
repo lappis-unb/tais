47ad32390dc8cc16e13c439791c4b964b1be0439
carla <rocha.carla@gmail.com>
2019-04-29 10:28:40 -0300

    intents + utters longas

--- coach/train.py
+++ coach/train.py
@@ -16,7 +16,7 @@ def train_dialogue(domain_file,
                                       stories_file=training_folder,
                                       output_path=model_path,
                                       policy_config=policy_config,
-                                      kwargs={'augmentation_factor': 5,
+                                      kwargs={'augmentation_factor': 1,
                                               'validation_split': 0.2}
                                       )
 
