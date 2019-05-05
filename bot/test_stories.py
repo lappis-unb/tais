c5eb0fb71b3238655474898ddb8a611eb7c2a760
Matheus Miranda <matheusmirandalacerda@gmail.com>
2019-05-05 10:11:32 -0300

    Add output to print Evaluation statistics
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- bot/test_stories.py
+++ bot/test_stories.py
@@ -1,19 +1,25 @@
 from rasa_core.evaluate import _generate_trackers, collect_story_predictions
 
+import logging
 from rasa_core import utils
 from rasa_core.utils import AvailableEndpoints
 from rasa_core.run import load_agent
 from rasa_core.interpreter import NaturalLanguageInterpreter
+from collections import namedtuple
 import re
+import sys
 import os
 from os import listdir
 from os.path import isfile, join
-
 import argparse
+
+
+logger = logging.getLogger(__name__)
 
 
 PASSED_COLOR = utils.bcolors.OKGREEN
 FAILED_COLOR = utils.bcolors.FAIL
+BLUE_COLOR = utils.bcolors.OKBLUE
 BOLD_COLOR = utils.bcolors.BOLD
 CORE_DIR = 'models/dialogue'
 NLU_DIR = 'models/nlu/current'
@@ -33,6 +39,11 @@ parser.add_argument(
 args = parser.parse_args()
 
 
+FileResult = namedtuple("FileResult",
+                        "num_stories "
+                        "num_failed_stories ")
+
+all_failed_stories = []
 files_results = {}
 
 
@@ -46,16 +57,18 @@ def is_an_intent_line(line):
 def process_failed_story(failed_story):
     print('\n')
     for line in failed_story.splitlines():
+        is_intent = is_an_intent_line(line)
+
         if re.search('<!--.*-->', line):
             error_prediction = re.search('<!--(.*)-->', line) \
                                  .group(1).strip().split(':')
             error_message = ''
 
-            if is_an_intent_line(line):
+            if is_intent:
                 error_message += '  Intent failed: Predicted intent' + \
                                  ('{} for message \'{}\''.
                                   format(error_prediction[1],
-                                         error_prediction[2])
+                                         error_prediction[2].strip())
                                   if len(error_prediction) == 3
                                   else error_prediction[1])
             else:
@@ -72,7 +85,7 @@ def process_failed_story(failed_story):
             print(line)
 
 
-def run_evaluation(stories_to_evaluate,
+def run_evaluation(file_to_evaluate,
                    fail_on_prediction_errors=False,
                    max_stories=None,
                    use_e2e=False):
@@ -83,7 +96,7 @@ def run_evaluation(stories_to_evaluate,
                         interpreter=_interpreter,
                         endpoints=_endpoints)
 
-    completed_trackers = _generate_trackers(stories_to_evaluate, _agent,
+    completed_trackers = _generate_trackers(file_to_evaluate, _agent,
                                             max_stories, use_e2e)
     story_evaluation, _ = collect_story_predictions(completed_trackers, _agent,
                                                     fail_on_prediction_errors,
@@ -91,48 +104,47 @@ def run_evaluation(stories_to_evaluate,
     _failed_stories = story_evaluation.failed_stories
 
     _num_stories = len(completed_trackers)
-    _file_result = [_num_stories, len(_failed_stories), []]
+    _file_result = FileResult(num_stories=_num_stories,
+                              num_failed_stories=len(_failed_stories))
 
-    file_message = "Evaluating stories for file {}".format(stories_to_evaluate)
+    file_message = "EVALUATING STORIES FOR FILE '{}':".format(file_to_evaluate)
     utils.print_color('\n' + '#' * 80, BOLD_COLOR)
     utils.print_color(file_message, BOLD_COLOR)
 
-    files_results[stories_to_evaluate] = _file_result
+    files_results[file_to_evaluate] = _file_result
 
     if len(_failed_stories) == 0:
-        success_message = 'All the stories have passed for {}!!' \
-                          .format(stories_to_evaluate)
-        utils.print_color('\n' + '=' * len(success_message), PASSED_COLOR)
-        utils.print_color(success_message, PASSED_COLOR)
-        utils.print_color('=' * len(success_message), PASSED_COLOR)
+        success_message = "The stories have passed for file '{}'!!" \
+                          .format(file_to_evaluate)
+        utils.print_color('\n' + '=' * len(success_message), BLUE_COLOR)
+        utils.print_color(success_message, BLUE_COLOR)
+        utils.print_color('=' * len(success_message), BLUE_COLOR)
     else:
         for failed_story in _failed_stories:
             process_failed_story(failed_story.export_stories())
             story_name = re.search('## (.*)',
                                    failed_story.export_stories()).group(1)
-            files_results[stories_to_evaluate][2].append(story_name)
+            all_failed_stories.append(file_to_evaluate + ' - ' + story_name)
 
     utils.print_color('#' * 80 + '\n', BOLD_COLOR)
 
 
-def print_files_results():
+def print_evaluation_statistics():
     utils.print_color('EVALUATION RESULTS:', BOLD_COLOR)
 
-    for stories_file, file_result in files_results.items():
-
-        _total_stories = str(file_result[0])
-        _passed_stories = str(file_result[0] - file_result[1])
-
-        utils.print_color('\n' + 'File - ' + stories_file, BOLD_COLOR)
-        utils.print_color('Passed: ' + _passed_stories + '/' +
-                          _total_stories, BOLD_COLOR)
+    if len(all_failed_stories) == 0:
+        utils.print_color('\n' + 'CONGRATULATIONS! ALL THE EVALUATED '
+                          'STORIES HAVE PASSED!', PASSED_COLOR)
+    else:
+        utils.print_color('\n' + 'FAILED STORIES:', FAILED_COLOR)
+        for failed_story in all_failed_stories:
+            utils.print_color(failed_story, FAILED_COLOR)
+        sys.exit(1)
 
 
 if __name__ == '__main__':
     stories_path = parser.parse_args().stories
     use_e2e = parser.parse_args().e2e
-
-    stories_to_evaluate = []
 
     if os.path.isdir(stories_path):
         if not stories_path.endswith('/'):
@@ -146,4 +158,4 @@ if __name__ == '__main__':
     else:
         run_evaluation(stories_path, False, None, use_e2e)
 
-    print_files_results()
+    print_evaluation_statistics()
