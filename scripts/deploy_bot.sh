d17dda12fe35a56eaf7d1ec8d7b129b994790c9a
Matheus Miranda <matheusmirandalacerda@gmail.com>
2019-05-06 16:40:57 -0300

    Update Gitlab CI config to new architecture
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- scripts/deploy_bot.sh
+++ scripts/deploy_bot.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sshpass  -p $1 ssh -o StrictHostKeyChecking=no root@$2 <<-'ENDSSH'
-		cd rouana/
-    docker-compose -f ./beta.docker-compose.yml stop bot
-		docker-compose -f ./beta.docker-compose.yml rm -f bot
-    docker rmi -f registry.gitlab.com/lappis-unb/services/tais/bot
-    docker-compose -f ./beta.docker-compose.yml up -d bot
+    cd rouana/
+    docker-compose stop bot
+		docker-compose rm -f bot
+		docker-compose pull bot
+    docker-compose up -d bot
 ENDSSH
