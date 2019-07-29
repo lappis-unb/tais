87d521ddfe44c9e8c8d0b162a886725643e91ad5
Matheus Miranda <matheusmirandalacerda@gmail.com>
2019-07-29 15:12:51 -0300

    Remove tracker from 'run-rocketchat' script
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- bot/run-rocketchat.py
+++ bot/run-rocketchat.py
@@ -4,6 +4,7 @@ import os
 from rasa_core.utils import configure_colored_logging, AvailableEndpoints
 from rasa_core.run import start_server, load_agent
 from rasa_core.interpreter import NaturalLanguageInterpreter
+from rasa_core.tracker_store import InMemoryTrackerStore
 
 from connector import RocketChatInput
 from tracker_store import ElasticTrackerStore
@@ -11,13 +12,21 @@ from tracker_store import ElasticTrackerStore
 logger = logging.getLogger(__name__)
 configure_colored_logging(loglevel='DEBUG')
 
+url = os.getenv('BROKER_URL', '')
+username = os.getenv('BROKER_USERNAME', '')
+password = os.getenv('BROKER_PASSWORD', '')
+queue = os.getenv('QUEUE_NAME', '')
+
+ENABLE_ANALYTICS = os.getenv('ENABLE_ANALYTICS', 'False').lower() == 'true'
 
 def run(core_dir, nlu_dir):
-    configs = {
-        'user': os.getenv('ROCKETCHAT_BOT_USERNAME'),
-        'password': os.getenv('ROCKETCHAT_BOT_PASSWORD'),
-        'server_url': os.getenv('ROCKETCHAT_URL'),
-    }
+    pika_broker = None
+
+    if ENABLE_ANALYTICS:
+        pika_broker = PikaProducer(url,
+                                   username,
+                                   password,
+                                   queue=queue)
 
     input_channel = RocketChatInput(
         user=configs['user'],
@@ -25,22 +34,10 @@ def run(core_dir, nlu_dir):
         server_url=configs['server_url']
     )
 
+    _tracker_store = InMemoryTrackerStore(event_broker=pika_broker)
+
     _endpoints = AvailableEndpoints.read_endpoints(None)
     _interpreter = NaturalLanguageInterpreter.create(nlu_dir)
-
-    elastic_user = os.getenv('ELASTICSEARCH_USER')
-    if elastic_user is None:
-        _tracker_store = ElasticTrackerStore(
-            domain=os.getenv('ELASTICSEARCH_URL', 'elasticsearch:9200')
-        )
-    else:
-        _tracker_store = ElasticTrackerStore(
-            domain=os.getenv('ELASTICSEARCH_URL', 'elasticsearch:9200'),
-            user=os.getenv('ELASTICSEARCH_USER', 'user'),
-            password=os.getenv('ELASTICSEARCH_PASSWORD', 'password'),
-            scheme=os.getenv('ELASTICSEARCH_HTTP_SCHEME', 'http'),
-            scheme_port=os.getenv('ELASTICSEARCH_PORT', '80')
-        )
 
     _agent = load_agent(core_dir,
                         interpreter=_interpreter,
