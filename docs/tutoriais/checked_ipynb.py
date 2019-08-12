a30a764371732159951dab53f4f9ffc216d4e710
LeoSilvaGomes <leonardodasigomes@gmail.com>
2019-08-12 17:34:43 -0300

    Create script to convert md to ipynb
    Co-authored-by: João Vitor <joaovytor0@gmail.com>

--- docs/tutoriais/checked_ipynb.py
+++ docs/tutoriais/checked_ipynb.py
@@ -3,34 +3,24 @@ import os
 import glob
 import logging
 
+logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s',
+                    datefmt='%d-%m-%Y %H:%M',
+                    level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def get_md_files():
     md_files = glob.glob('*.md')
     return md_files
 
-notebook_files = [md_file.replace('md', 'ipynb') for md_file in get_md_files()]
-missing_files = []
-
-for i, notebook_file in enumerate(notebook_files):
-    if not os.path.isfile("notebooks/" + notebook_file):
-        missing_files.append(md_files[i])
-        os.system("notedown " + md_files[i] + " > notebooks/" + notebook_file)
-
-
-for notebook_file in notebook_files:
-    with open('notebooks/' + notebook_file) as fjason:
-        jason_file = json.load(fjason)
-
-    for i, cell in enumerate(jason_file['cells']):
-        try:
-            if cell['cell_type'] == 'markdown' and cell['metadata']['lang'] != 'pt':
-                del jason_file['cells'][i]
-        except KeyError:
-            logger.error(notebook_file + " Untranslated")
-
-    with open('tmp.ipynb', 'w') as f:
-        json.dump(jason_file, f)      
-
-    os.system("notedown tmp.ipynb --to markdown --strip > " + notebook_file.replace('ipynb', 'md'))
-    os.system("rm -r tmp.ipynb")
+def md_to_ipynb(notebook_files, md_files):
+    missing_files = []
+    for i, notebook_file in enumerate(notebook_files):
+        if not os.path.isfile("notebooks/" + notebook_file):
+            missing_files.append(md_files[i])
+            os.system("notedown " + md_files[i] + " > notebooks/" + notebook_file)
+            
+if __name__ == "__main__":
+    logger.info("Checking notebooks process started")
+    md_files = get_md_files()
+    notebook_files = [md_file.replace('md', 'ipynb') for md_file in get_md_files()]
+    md_to_ipynb(notebook_files, md_files)
